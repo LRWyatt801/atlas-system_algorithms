10297a453687dd862322fbec53c3295516565485
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-12 20:01:02 -0500

    got frustrated calling it a night

--- red_black_tree/0-rb_tree_node.c
+++ red_black_tree/0-rb_tree_node.c
@@ -22,8 +22,7 @@ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->color = color;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new_node->left = new_node->right = NULL;
 
 	return (new_node);
 }
