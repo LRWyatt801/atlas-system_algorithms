a3fd96f539511a4972b72e42cadcced917a7bf6f
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-23 13:10:34 -0500

    Rearrange code to make betty happy and add duplicate str frees

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int init_vertex(vertex_t **, const char *);
+
 /**
  * graph_add_vertex - adds a vertex(node) to an existing graph
  * @graph: pointer to the graph to add the vertex to
@@ -25,17 +27,10 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 		perror("graph_add_vertex: malloc error");
 		return (NULL);
 	}
-	new_vertex->index = 0;
-	new_vertex->content = strdup(str);
-	if (!new_vertex->content)
-	{
-		free(new_vertex);
-		perror("graph_add_vertex: strdup");
+
+	if (init_vertex(&new_vertex, str) == -1)
 		return (NULL);
-	}
-	new_vertex->nb_edges = 0;
-	new_vertex->edges = NULL;
-	new_vertex->next = NULL;
+
 	if (!graph->vertices)
 		graph->vertices = new_vertex;
 	else
@@ -44,15 +39,44 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 		while (tmp->next != NULL)
 		{
 			if (strcmp(tmp->content, str) == 0)
-				return (NULL);
+				goto cleanup;
 			tmp = tmp->next;
 			i++;
 		}
 		if (strcmp(tmp->content, str) == 0)
-			return (NULL);
+			goto cleanup;
 		new_vertex->index = i + 1;
 		tmp->next = new_vertex;
 	}
 	graph->nb_vertices++;
 	return (new_vertex);
+
+cleanup:
+	free(new_vertex->content);
+	free(new_vertex);
+	return (NULL);
+}
+
+/**
+ * init_vertex - inits a vertex with given values
+ * @vertex: vertex to init
+ * @str: string to init content with
+ *
+ * Return: 0 on success, otherwise -1
+ */
+
+int init_vertex(vertex_t **vertex, const char *str)
+{
+	(*vertex)->index = 0;
+	(*vertex)->content = strdup(str);
+	if (!(*vertex)->content)
+	{
+		free(vertex);
+		perror("init_vertex: strdup");
+		return (-1);
+	}
+	(*vertex)->nb_edges = 0;
+	(*vertex)->edges = NULL;
+	(*vertex)->next = NULL;
+	return (0);
 }
