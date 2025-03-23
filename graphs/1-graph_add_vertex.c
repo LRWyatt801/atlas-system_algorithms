cfe78f66230357ee736b2cfbf43fd5edb92639e1
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-23 11:36:20 -0500

    add check for dup

--- graphs/1-graph_add_vertex.c
+++ graphs/1-graph_add_vertex.c
@@ -44,15 +44,12 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 		while (tmp->next != NULL)
 		{
 			if (strcmp(tmp->content, str) == 0)
-			{
-				free(new_vertex->content);
-				free(new_vertex);
-				printf("Failed to add [%s]\n", str);
 				return (NULL);
-			}
 			tmp = tmp->next;
 			i++;
 		}
+		if (strcmp(tmp->content, str) == 0)
+			return (NULL);
 		new_vertex->index = i + 1;
 		tmp->next = new_vertex;
 	}
