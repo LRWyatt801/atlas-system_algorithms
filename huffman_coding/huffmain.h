1958240993f5a4945bac2c5166d9c732ba65f147
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-30 15:41:19 -0500

    Add provided prototrypes

--- huffman_coding/huffmain.h
+++ huffman_coding/huffmain.h
@@ -1,6 +1,8 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+#include "heap/heap.h"
+
 #include <stdio.h>
 
 /*****************************************************************************/
@@ -19,6 +21,12 @@ typedef struct symbol_s
 } symbol_t;
 
 /************************ PROVIDED PROTOTYPES ********************************/
+
+symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+int huffman_extract_and_insert(heap_t *priority_queue);
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+int huffman_codes(char *data, size_t *freq, size_t size);
 
 /*****************************************************************************/
 
