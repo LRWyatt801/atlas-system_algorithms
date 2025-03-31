f15b2f9fc556c0c36ad9a57971bdc837f8aaa51f
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-31 13:34:40 -0500

    Fix betty

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -2,12 +2,20 @@
 
 #include <stdlib.h>
 
+/**
+ * heap_create - create a binary heap
+ * @data_cmp: funtion pointer to compare data
+ *
+ * Return: pointer to newly created heap, or NULL on failure
+ */
+
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
 	if (!data_cmp)
 		return (NULL);
 
 	heap_t *new_heap = malloc(sizeof(heap_t));
+
 	if (!new_heap)
 		return (NULL);
 
