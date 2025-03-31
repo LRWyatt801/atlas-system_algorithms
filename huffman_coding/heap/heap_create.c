fcb817360a3047b981db5c9bd8172325668f634c
LRWyatt801 <loganwyatt1995@gmail.com>
2025-03-31 13:31:49 -0500

    Heap create

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,6 +1,19 @@
 #include "heap.h"
 
+#include <stdlib.h>
+
 heap_t *heap_create(int (*data_cmp)(void *, void *))
 {
+	if (!data_cmp)
+		return (NULL);
 
+	heap_t *new_heap = malloc(sizeof(heap_t));
+	if (!new_heap)
+		return (NULL);
+
+	new_heap->root = NULL;
+	new_heap->size = 0;
+	new_heap->data_cmp = data_cmp;
+
+	return (new_heap);
 }
