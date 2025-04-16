d7fe803c428ab5dd0b049bf142207e4bacf9d7ca
LRWyatt801 <loganwyatt1995@gmail.com>
2025-04-15 21:17:15 -0500

    push for the night

---
+++ pathfinding/0-backtracking_array.c
@@ -0,0 +1,19 @@
+#include "pathfinding.h"
+
+/**
+ * backtracking_array - searches for the first path from a starting point to a
+ *		       target point within a two-dimensional array
+ * @map: pointer to a read only 2d array
+ * @rows: number of rows
+ * @cols: number of columns
+ * @start: coordinates of starting point
+ * @target: coordinates of target point
+ *
+ * Return: a queue in which each node is point in the path from start to target
+ */
+
+queue_t *backtracking_array(char **map, int rows, int cols,
+                            point_t const *start, point_t const *target)
+{
+
+}
