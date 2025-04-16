fa91168bdd92fbac0aa77eae79d38b10ffc86716
LRWyatt801 <loganwyatt1995@gmail.com>
2025-04-15 21:23:47 -0500

    fix betty

--- pathfinding/0-backtracking_array.c
+++ pathfinding/0-backtracking_array.c
@@ -13,7 +13,7 @@
  */
 
 queue_t *backtracking_array(char **map, int rows, int cols,
-                            point_t const *start, point_t const *target)
+			    point_t const *start, point_t const *target)
 {
-
+	return (NULL);
 }
--- pathfinding/pathfinding.h
+++ pathfinding/pathfinding.h
@@ -15,15 +15,18 @@
  */
 typedef struct point_s
 {
-    int x;
-    int y;
+	int x;
+	int y;
 } point_t;
 
 /******************************** PROVIDED PROTOTYPES ************************/
 
-queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
-queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
-queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+queue_t *backtracking_array(char **map, int rows, int cols,
+							point_t const *start, point_t const *target);
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+							vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+						vertex_t const *target);
 
 /*****************************************************************************/
 
